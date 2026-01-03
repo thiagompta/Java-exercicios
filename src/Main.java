0d327d128362d9f37ef8833ff52a398b58b2b03d
Thiago Silva <thiagompta008@hotmail.com>
2026-01-03 04:04:52 -0300

    Um progama que le um numero e diz se é par ou impar

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         String texto = """
-                Digite 1 numero para ver a tabuada do mesmo
+                Digite 1 numero
                 """;
         System.out.print(texto);
-        int contador = 1;
         int numero = leitor.nextInt();
-        while (contador >= 0 && contador <= 10){;
-            int resultado = numero * contador;
-            System.out.println( numero + "x" + contador + "=" + resultado);
-            contador++;
+        if (numero % 2 == 0){
+            System.out.println("O numero é par");
+        }else{
+            System.out.println("O numero é impar");
         }
+
     }
     }
